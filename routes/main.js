b89e6030eee886fd6d02f003a082b05091be4f19
dahyezzz <dpswjfepsl@naver.com>
2024-11-26 13:33:59 +0900

    first commit

---
+++ routes/main.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const mainLayout = "../views/layouts/main.ejs";
+const Post = require("../models/Post");
+const asyncHandler = require("express-async-handler");
+
+router.get(
+  ["/", "/home"],
+  asyncHandler(async (req, res) => {
+    const locals = {
+      title: "Home",
+    };
+    const date = await Post.find();
+    res.render("index", { locals, date, layout: mainLayout });
+  })
+);
+
+router.get("/about", (req, res) => {
+  const locals = {
+    title: "About",
+  };
+  res.render("about", { locals, layout: mainLayout });
+});
+
+/**
+ * 게시물 상세보기
+ * GET /post/:id
+ */
+router.get(
+  "/post/:id",
+  asyncHandler(async (req, res) => {
+    const date = await Post.findOne({ _id: req.params.id });
+    res.render("post", { date, layout: mainLayout });
+  })
+);
+module.exports = router;
+
+// Post.insertMany( [
+//   {
+//   title: "제목-1",
+//   body: "내용-1 Lorem ipsum dolor, sit amet consectetur elit. Iure a inventore officia magni fugit dignissimos animi quas explicabo! Rerum voluptatibus asperiores illum ducimus autem itaque unde aliquid lavorum numquam. Hic!"
+// },
+// {
+//   title: "제목-2",
+//   body: "내용-2 Lorem ipsum dolor, sit amet consectetur elit. Iure a inventore officia magni fugit dignissimos animi quas explicabo! Rerum voluptatibus asperiores illum ducimus autem itaque unde aliquid lavorum numquam. Hic!"
+// },
+// {
+//   title: "제목-3",
+//   body: "내용-3 Lorem ipsum dolor, sit amet consectetur elit. Iure a inventore officia magni fugit dignissimos animi quas explicabo! Rerum voluptatibus asperiores illum ducimus autem itaque unde aliquid lavorum numquam. Hic!"
+// },  {
+//   title: "제목-4",
+//   body: "내용-4 Lorem ipsum dolor, sit amet consectetur elit. Iure a inventore officia magni fugit dignissimos animi quas explicabo! Rerum voluptatibus asperiores illum ducimus autem itaque unde aliquid lavorum numquam. Hic!"
+// },  {
+//   title: "제목-5",
+//   body: "내용-5 Lorem ipsum dolor, sit amet consectetur elit. Iure a inventore officia magni fugit dignissimos animi quas explicabo! Rerum voluptatibus asperiores illum ducimus autem itaque unde aliquid lavorum numquam. Hic!"
+// },  {
+//   title: "제목-6",
+//   body: "내용-6 Lorem ipsum dolor, sit amet consectetur elit. Iure a inventore officia magni fugit dignissimos animi quas explicabo! Rerum voluptatibus asperiores illum ducimus autem itaque unde aliquid lavorum numquam. Hic!"
+// }
+
+// ]);
